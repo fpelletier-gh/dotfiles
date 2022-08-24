a1d5327822c85c1c36754ec66f01f0e78db49879
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-23 22:14:01 -0400

    add formatting and linting for python and django

--- config/nvim/lua/user/lsp/null-ls.lua
+++ config/nvim/lua/user/lsp/null-ls.lua
@@ -7,7 +7,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
@@ -15,8 +15,10 @@ null_ls.setup({
 		-- formatting
 		formatting.prettier,
 		formatting.stylua,
+		formatting.black,
+		formatting.djhtml,
 		-- diagnostics
-		-- diagnostics.eslint,
+		diagnostics.pylint,
 	},
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
