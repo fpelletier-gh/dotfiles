bee0e7a8f4a7937e8afc70a98f7a7e13c7262de4
Francis Pelletier <francispelletier2@hotmail.com>
2023-01-01 13:47:33 -0500

    add docker engine to install script

--- install/apps.sh
+++ install/apps.sh
@@ -54,6 +54,18 @@ echo -e "\\n\\nInstalling Z\\n"
 git clone https://github.com/rupa/z.git ~/z
 touch ~/.z
 
+echo -e "\\n\\nInstalling Docker engine\\n"
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
+
 echo -e "\\n\\nInstalling python for neovim\\n"
 python3 -m pip install --upgrade pynvim
 
