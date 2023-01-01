fae91a6ff5274f20030b5df98d9e7e8b68006df0
Francis Pelletier <francispelletier2@hotmail.com>
2023-01-01 14:37:14 -0500

    add protected call for impatient.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,8 @@
-require("impatient")
+local status_ok = pcall(require, "impatient")
+if not status_ok then
+	return
+end
+
 require("user.options")
 require("user.plugins")
 require("user.colorscheme")
